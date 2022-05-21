051fed9710600b4c55d92af957d972a9bd6f6f27
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-05-21 09:59:25 +0300

    Add new Folder

---
+++ 04. Multidimentional Arrays - Exercise/P05. Snake Moves/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace P05._Snake_Moves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDemension = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            char[,] matrix = new char[matrixDemension[0], matrixDemension[1]];
+            string snake = Console.ReadLine();  
+
+            int index = 0;  
+
+            for (int r = 0; r < matrixDemension[0]; r++)
+            {
+                if (r%2==0)
+                {
+                    for (int c = 0; c < matrixDemension[1]; c++)
+                    {
+                        matrix[r, c] = snake[index];
+                        index++;
+                        if (index>=snake.Length)
+                        {
+                            index = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int c = matrixDemension[1]-1; c >=0; c--)
+                    {
+                        matrix[r,c] = snake[index];
+                        index++;
+                        if (index>=snake.Length)
+                        {
+                            index = 0;
+                        }
+                    }
+                }
+               
+            }
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    Console.Write(matrix[r, c]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
