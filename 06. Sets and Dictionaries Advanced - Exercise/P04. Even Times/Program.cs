9da92a82a1639939df9dcb84a9d92123e0481f53
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-05-25 17:43:38 +0300

    Add new Folder

---
+++ 06. Sets and Dictionaries Advanced - Exercise/P04. Even Times/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (map.ContainsKey(num))
+                {
+                    map[num]++;
+                }
+                else
+                {
+                    map.Add(num, 1);
+                }
+            }
+            foreach (var item in map)
+            {
+                if (item.Value%2==0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+            
+        }
+    }
+}
