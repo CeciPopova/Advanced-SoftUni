02a2eaf9014f61277fd6e72526da82da5770cbee
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-05-26 18:27:20 +0300

    Add new Folder

---
+++ 07. Streams, Files and Directories - Lab/P02. Line Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace P02._Line_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           using StreamReader reader = new StreamReader(@"C:\Temp\input.txt");
+           using StreamWriter writer = new StreamWriter(@"C:\Temp\output.txt");
+
+            int count = 1;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                writer.WriteLine($"{count}. {line}");
+                Console.WriteLine($"{count}. {line}");
+                count++;    
+            }
+        }
+    }
+}
