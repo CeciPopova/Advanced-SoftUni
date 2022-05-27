df2eb4b8fca594307693f33286e65eaa9ec24c76
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-05-27 13:03:33 +0300

    Add new Folder

---
+++ 07. Streams, Files and Directories - Lab/P01. Odd Lines/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace OddLines
+{
+    using System;
+    using System.IO;
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\input.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            ExtractOddLines(inputPath, outputPath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+
+            using (StreamReader reader = new StreamReader(@"input.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter(@"../../../output.txt"))
+                {
+                    int counter = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        if (counter % 2 == 0)
+                        {
+                            writer.WriteLine(line);
+                            Console.WriteLine(line);
+                        }
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
