bf314f2bf95e0ab266c728d5c30e9b36c667fef1
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-05-27 13:06:36 +0300

    Add new Folder

---
+++ 07. Streams, Files and Directories - Lab/P02. Line Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = @"..\..\..\input.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using StreamReader reader = new StreamReader(@"input.txt");
+            using StreamWriter writer = new StreamWriter(@"output.txt");
+
+            int count = 1;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                writer.WriteLine($"{count}. {line}");
+               
+                count++;
+            }
+        }
+    }
+}
