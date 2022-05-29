077e9a27dc9337b55242809375d1c5d93c853d16
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-05-29 09:28:56 +0300

    Add new Folders

---
+++ 08. Streams, Files and Directories - Exercises/P01. Even Lines/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            ProcessLines(inputFilePath);
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                int count = 0;
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (count % 2 == 0)
+                    {
+                        line = Replace(line);
+                        line = ReverseLine(line);
+                        Console.WriteLine(line);
+
+                    }
+
+                    line = reader.ReadLine();
+                    count++;
+                }
+                return line;
+            }
+
+        }
+        private static string ReverseLine(string line)
+        {
+            return string.Join(" ", line.Split(' ').Reverse());
+        }
+        private static string Replace(string line)
+        {
+            return line.Replace("-", "@")
+                .Replace(",", "@")
+                .Replace(".", "@")
+                .Replace("!", "@")
+                .Replace("?", "@");
+        }
+    }
+}
