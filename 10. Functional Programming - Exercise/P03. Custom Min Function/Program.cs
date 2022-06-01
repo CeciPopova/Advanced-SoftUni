0c066fd510db5dcc0c97006c75d2994fd0f2f203
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-06-01 19:22:51 +0300

    Add new Folders

---
+++ 10. Functional Programming - Exercise/P03. Custom Min Function/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            Func<List<int>,int> getMinElement = nums => nums.Min();
+            Console.WriteLine(getMinElement(nums));
+        }
+    }
+}
---
+++ 10. Functional Programming - Exercise/P04. Find Evens or Odds/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04._Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bounds = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int start = bounds[0];
+            int end = bounds[1];  
+          
+            List<int> list = new List<int>();
+            Predicate<int> predicate =null;
+            for (int i = start ; i <= end; i++)
+            {
+                list.Add(i);
+               
+            }
+            string command = Console.ReadLine();
+            if (command == "odd")
+            {
+                predicate = n => n % 2 != 0;
+            }
+            else if (command == "even")
+            {
+                predicate = n => n % 2 == 0;
+            }
+          
+            Console.WriteLine(string.Join(" ",list.FindAll(predicate)));  
+
+        }
+    }
+}
