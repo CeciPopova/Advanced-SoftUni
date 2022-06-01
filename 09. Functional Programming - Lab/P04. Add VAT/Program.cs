8827720cc6326e3b6c544ed0e27f275976a9e587
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-06-01 16:48:50 +0300

    Add new Folder

---
+++ 09. Functional Programming - Lab/P04. Add VAT/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace P04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] prices = Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse).ToArray();
+            prices=prices.Select(n=>n+n*0.2).ToArray();
+            foreach (var price in prices)
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+        }
+    }
+}
