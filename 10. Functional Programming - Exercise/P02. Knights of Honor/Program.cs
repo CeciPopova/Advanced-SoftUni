f6beea177d985d80d5006ec18afc8c37b16e0c52
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-06-01 18:45:45 +0300

    Rename Folder

---
+++ 10. Functional Programming - Exercise/P02. Knights of Honor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+
+            Action<string> printStrings = x => Console.WriteLine("Sir" + " " +x);
+            for (int i = 0; i < input.Length; i++)
+            {
+                printStrings(input[i]);
+            }
+        }
+    }
+}
