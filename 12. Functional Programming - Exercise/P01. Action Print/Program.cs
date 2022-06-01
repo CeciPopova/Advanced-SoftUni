3cca9a999ef433bd65e60e4be319dd1b2413cc38
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-06-01 18:40:22 +0300

    Add new Folders

---
+++ 12. Functional Programming - Exercise/P01. Action Print/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace P01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] input = Console.ReadLine().Split(' ').ToArray();
+            Action<string> printStrings = x => Console.WriteLine(x);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                printStrings(input[i]);
+            }
+        }
+    }
+}
