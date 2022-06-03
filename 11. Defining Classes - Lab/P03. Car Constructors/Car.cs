d1547bdb142e7f676a9874635cf6f8813755a09b
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-06-03 11:59:33 +0300

    Add new Folder

---
+++ 11. Defining Classes - Lab/P03. Car Constructors/Car.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        private double fuelQuantity;
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        private double fuelConsumption;
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public Car()
+        {
+            this.make = "VW";
+            this.model = "Golf";
+            this.year = 2025;
+            this.fuelQuantity = 200;
+            this.fuelConsumption = 10;
+        }
+        public Car(string make,string model,int year)
+            :this()
+        {
+            this.Make = make;
+            this.Model = model; 
+            this.Year = year;   
+        }
+
+        public Car(string make,string model, int year,double fuelQuantity,double fuelConsumption)
+            :this(make,model,year)  
+        {
+            this.FuelQuantity = fuelQuantity; 
+            this.FuelConsumption = fuelConsumption; 
+        }
+
+        public void Drive(double distance)
+        {
+            double consumption = distance * this.FuelConsumption;
+            if (consumption <= this.FuelQuantity)
+            {
+                this.fuelQuantity -= consumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:f2}L";
+        }
+    }
+}
+
