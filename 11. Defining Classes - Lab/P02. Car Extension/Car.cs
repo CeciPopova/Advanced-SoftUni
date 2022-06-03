688a2b6b085ed3f5cbd63731fe6225a5426aa488
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-06-03 10:10:36 +0300

    Add new Folder

---
+++ 11. Defining Classes - Lab/P02. Car Extension/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        private string make;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        private double fuelQuantity;
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        private double fuelConsumption;
+
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            double consumption = distance * this.FuelConsumption;
+            if (consumption<=this.FuelQuantity)
+            {
+                this.fuelQuantity -= consumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:f2}L";
+        }
+    }
+}
+
