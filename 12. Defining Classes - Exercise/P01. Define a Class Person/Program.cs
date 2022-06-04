e2f8b1f2bb9090ae7c629af193ba754367c982a7
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-06-04 04:25:08 +0300

    Add new Folders

---
+++ 12. Defining Classes - Exercise/P01. Define a Class Person/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string name = "Peter";
+            int age = 20;
+            Person person = new Person(name, age);
+           
+        }
+    }
+}
---
+++ 12. Defining Classes - Exercise/P02. Creating Constructors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person firstPersin = new Person();  
+            Person secondPersin = new Person(7);
+            Person thirdPersin = new Person("Victor",9);
+
+            Console.WriteLine($"First person is {firstPersin.Name} - {firstPersin.Age} years old!");
+            Console.WriteLine($"Second person is {secondPersin.Name} - {secondPersin.Age} years old!");
+            Console.WriteLine($"Third person is {thirdPersin.Name} - {thirdPersin.Age} years old!");
+            
+        }
+    }
+}
