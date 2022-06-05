63fff28ff2bcb6e8fa09a225c5993160065e5af8
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-06-05 23:28:30 +0300

    Add Folder

---
+++ 12. Defining Classes - Exercise/P08. Car Salesman2/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public int Weight { get; set; }
+        public string Color { get; set; }
+
+        public Car(string model,Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{Model}:");
+            sb.AppendLine($"  {Engine.Model}:");
+            sb.AppendLine($"    Power: {Engine.Power}");
+            sb.AppendLine(Engine.Displacement == 0 ? "    Displacement: n/a" : $"    Displacement: {Engine.Displacement}");
+            sb.AppendLine(Engine.Efficiency == null ? "    Efficiency: n/a" : $"    Efficiency: {Engine.Efficiency}");
+            sb.AppendLine(Weight == 0 ? "  Weight: n/a" : $"  Weight: {Weight}");
+            sb.Append(Color == null ? "  Color: n/a" : $"  Color: {Color}");
+            return sb.ToString();
+        }
+    }
+}
