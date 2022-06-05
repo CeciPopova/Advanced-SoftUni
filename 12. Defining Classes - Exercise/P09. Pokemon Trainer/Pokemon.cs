beb9e5c7bda5e40aba8b186103ecf630acde2fc0
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-06-05 23:29:53 +0300

    Add Folder

---
+++ 12. Defining Classes - Exercise/P09. Pokemon Trainer/Pokemon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Pokemon
+    {
+        //-------------- Constructors --------------
+        public Pokemon(string name, string element, int health)
+        {
+            this.Name = name;
+            this.Element = element;
+            this.Health = health;
+        }
+
+        //--------------- Properties ---------------
+        public string Name { get; private set; }
+
+        public string Element { get; private set; }
+
+        public int Health { get; private set; }
+
+        //---------------- Methods -----------------
+        public void RemoveHealth()
+        {
+            this.Health -= 10;
+        }
+    }
+}
+    
+
