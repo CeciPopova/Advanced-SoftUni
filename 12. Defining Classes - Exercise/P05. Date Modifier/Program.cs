eab84a80188b3bff9262703bc9e789136f1827b5
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-06-05 09:28:42 +0300

    Add new Folder

---
+++ 12. Defining Classes - Exercise/P05. Date Modifier/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            DateModifier difference = new DateModifier();
+            TimeSpan diff = difference.GetTheDifference(startDate, endDate);
+            Console.WriteLine(diff.Days.ToString());
+
+
+        }
+    }
+}
